28d620ea4a9fb1cd2815d7e664f07fc4cf461158
cecileung1208 <71020959+cecileung1208@users.noreply.github.com>
2021-01-18 00:13:49 -0500

    added otu_labels part

--- Belly Button Biodiversity/static/js/app.js
+++ Belly Button Biodiversity/static/js/app.js
@@ -7,26 +7,44 @@ function barPlot() {
 
     var samples_list = data.samples[0].sample_values;
     console.log(samples_list);
+
+    var otu_labels_list = data.samples[0].otu_labels;
+    console.log(otu_labels_list)
     
     var otu_id = otu_list.slice(0,10).reverse();
     console.log(otu_id);
 
+    var otu_id = otu_id.map(row => "OTU" + row)
+    console.log(otu_id)
+
     var samples = samples_list.slice(0,10).reverse();
     console.log(samples)
 
-    var labels = otu_id.map(row => "OTU" + row)
+    var labels = otu_labels_list.slice(0,10).reverse();
     console.log(labels)
+
+  
+
+    
 
     var trace1 = {
       x: samples,
-      y: labels,
+      y: otu_id,
+      text: labels, 
       type:"bar",
       orientation: "h",
     }
 
     var data = [trace1];
 
-    var layout = {title: "Top 10 OTU"}
+    var layout = {title: "Top 10 OTU",
+                  margin: {
+                            l: 100,
+                            r: 100,
+                            t: 100,
+                            b: 100
+                          },
+                  };
 
     Plotly.newPlot("bar", data, layout);
 
